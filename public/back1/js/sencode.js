5953366cf996f84d80ff88521a171b9bb73eda01
Aerfajj <15738665175@163.com>
2018-11-29 11:13:08 +0800

    二级分类模态框请求一级分类完成

--- public/back1/js/sencode.js
+++ public/back1/js/sencode.js
@@ -1,22 +1,23 @@
 /****设置二级列表 */
 
 $(function () {
+  var currentPage=1 ;
+  var pageSize =5;
 
+  //初始化渲染表单
   function rend() {
-    var currentPage ;
-    var pageSize =5;
-
-
+   
     //发送ajax请求
     $.ajax({
       url: '/category/querySecondCategoryPaging',
       type: 'get',
+      
       data: {
-        page: currentPage || 1,
+        page: currentPage,
         pageSize: pageSize
       },
       success: function (info) {
-
+        console.log(currentPage);
         console.log(info);
         var html = template('tml', info);
         $('tbody').html(html);
@@ -46,6 +47,38 @@ $(function () {
 
   }
   rend();
+
+  //注册点击事件
+  //1.点击按钮显示模态框
+  //2.发送ajax请求获取一级分类的数据
+  //3.准备模板引擎，将获取到的一级分类的信息渲染在ul部分并隐藏该区域
+  //4.点击选择图片，根据inupt的框的改变，获取url地址
+  //5.点击添加，将所有信息提交
+  $('.addBtn').click(function () {
+    //显示模态框
+    $('#cartModal').modal('show');
+    // alert('触发');
+
+    //发送ajax请求，获取一级菜单的内容
+    $.ajax({
+      url:'/category/queryTopCategoryPaging',
+      data:{
+        page: 1,
+        pageSize:10
+      },
+      type:'get',
+      success:function (info) {
+        
+        // console.log(info);
+        var html = template('firest_tml',info);
+        $('.dropdown-menu').html(html);
+        
+      }
+    });
+    
+  });
+
+
 
 
 });
